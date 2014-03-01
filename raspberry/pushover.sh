2805f007bafa14cd6ecc02e75efa56724d4424bb
giovanebribeiro <giovanebribeiro@gmail.com>
2014-03-01 20:53:40 -0300

    Ajustes do pushover.sh

--- raspberry/pushover.sh
+++ raspberry/pushover.sh
@@ -14,7 +14,6 @@
 ##
 
 CONF_FILE="$HOME/.pushover.conf.d"
-NUM_PARAMS=$#
 
 #
 #
@@ -26,14 +25,17 @@ echo "* Script file to send messages to Pushover"
 echo "* Author: Giovane Boaviagem"
 echo "* For more informations: http://pushover.net"
 echo "*"
+echo "* For the first use: $0 it"
 echo "* Usage: $0 [options] [parameters]"
 echo "* Options:"
+echo "* 	it: Init the configuration files"
 echo "*		hp: Displays this message"
 echo "*		ad <app_name>: Adds a new app name/key to database"
 echo "*		rm <app_name>: Remove the app_name to database"
 echo "*		sd <app_name> <title> <message>: Send a message with title"
 echo "*"
 echo "* Examples:"
+echo "*		$0 it"
 echo "*		$0 hp"
 echo "*		$0 ad <app_name>"
 echo "*		$0 rm <app_name>"
@@ -46,9 +48,9 @@ echo "************************************************"
 #
 add_api_key(){
 if [ ! $# -eq 1 ] ; then
-echo "Invalid # of parameters"
-_help
-exit
+	echo "Invalid # of parameters"
+	_help
+	exit
 fi
 
 API_NAME=$1
@@ -84,59 +86,67 @@ esac
 #
 #
 send(){
-if [ ! $# -eq 3 ] ; then
-	echo "Invalid # of parameters"
-	_help
+	if [ ! $# -eq 3 ] ; then
+		echo "Invalid # of parameters"
+		_help
+		exit
+	fi
+
+	APP_NAME="$1"
+	TITLE="$2"
+	MESSAGE="$3"
+	USER_KEY=`cat $CONF_FILE/_USER_KEY`
+	DEVICE=`cat $CONF_FILE/_DEVICE`
+	API_KEY=`cat $CONF_FILE/$APP_NAME`
+
+	curl \
+	-F "token=${API_KEY}" \
+	-F "user=${USER_KEY}" \
+	-F "device=${DEVICE}" \
+	-F "title=${TITLE}" \
+	-F "message=${MESSAGE}" \
+	"https://api.pushover.net/1/messages.json" > /dev/null 2>&1
+}
+
+init(){
+	if [ ! -e $CONF_FILE ] ; then
+		mkdir $CONF_FILE
+		echo "********************************************************"
+		echo "* Pushover script"
+		echo "* Access your profile in http://pushover.net for more"
+		echo "* informations to obtain your keys."
+		echo "*"
+		echo -n "* User key: "
+		read USER_KEY
+		echo "$USER_KEY" > $CONF_FILE/_USER_KEY
+		echo -n "* Device name [optional]: "
+		read DEVICE
+		if [ -z $DEVICE ] ; then
+			DEVICE=""
+		fi
+		echo "$DEVICE" >> $CONF_FILE/_DEVICE
+		while true; do
+			echo -n "* Would you like to add api keys (y/n)?"
+			read option
+			case $option in
+				y) add_api_key ;;
+				n) break ;;
+				*) echo "** Incorrect option." ;;
+			esac
+		done
+		echo "********************************************************"
+		exit
+	fi
+}
+
+if [ ! -z $CONF_FILE ] ; then
+	echo "Configuration files isn't created!"
+	init
 	exit
 fi
 
-APP_NAME="$1"
-TITLE="$2"
-MESSAGE="$3"
-USER_KEY=`cat $CONF_FILE/_USER_KEY`
-DEVICE=`cat $CONF_FILE/_DEVICE`
-API_KEY=`cat $CONF_FILE/$APP_NAME`
-
-curl \
--F "token=${API_KEY}" \
--F "user=${USER_KEY}" \
--F "device=${DEVICE}" \
--F "title=${TITLE}" \
--F "message=${MESSAGE}" \
-"https://api.pushover.net/1/messages.json" > /dev/null 2>&1
-}
-
-if [ ! -e $CONF_FILE ] ; then
-_help
-mkdir $CONF_FILE
-#echo "********************************************************"
-echo "* Pushover script"
-echo "* Access your profile in http://pushover.net for more"
-echo "* informations to obtain your keys."
-echo "*"
-echo -n "* User key: "
-read USER_KEY
-echo "$USER_KEY" > $CONF_FILE/_USER_KEY
-echo -n "* Device name [optional]: "
-read DEVICE
-if [ -z $DEVICE ] ; then
-	DEVICE=""
-fi
-echo "$DEVICE" >> $CONF_FILE/_DEVICE
-while true; do
-echo -n "* Would you like to add api keys (y/n)?"
-read option
-case $option in
-	y) add_api_key ;;
-	n) break ;;
-	*) echo "** Incorrect option." ;;
-esac
-done
-echo "********************************************************"
-exit
-fi
-
 case $1 in
+	it) init ;;
 	hp) _help ;;
 	ad) add_api_key "$2";;
 	rm) rm_api_key "$2";;
